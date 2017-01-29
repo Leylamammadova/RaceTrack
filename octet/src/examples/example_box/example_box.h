15558086ff3a56b1422b7f4d47f037aaba9f0c20
Abdullah Mukhtar Mansur Bin Abdullah <yuureijin@gmail.com>
2017-01-29 12:37:16 +0000

    Separated the points generator into its own class.

--- octet/src/examples/example_box/example_box.h
+++ octet/src/examples/example_box/example_box.h
@@ -6,6 +6,7 @@
 //
 #include <stdlib.h>
 #include <ctime>
+#include "points_generator.h"
 
 
 namespace octet {
@@ -39,18 +40,9 @@ namespace octet {
       return out;
     }
 
-    float RandomFloat(float a, float b) {
-      float random = ((float)rand()) / (float)RAND_MAX;
-      float diff = b - a;
-      float r = random * diff;
-      return a + r;
-    }
-
-    /// Vector of points
+    points_generator pg;
     int num_points;
     std::vector<vec3> waypoints;
-    std::vector<vec3> sorted_waypoints;
-
 
   public:
     /// this is called when we construct the class before everything is initialised.
@@ -70,9 +62,8 @@ namespace octet {
       std::srand(std::time(0));
 
       // create points for curves
-      generate_random_points();
-      sort_waypoints();
-      average_waypoints();
+      num_points = 112;
+      waypoints = pg.generate_random_points(num_points);
 
       float TRACK_WIDTH = 0.1f;
       float DETAIL_STEP = 0.00001f;
@@ -100,59 +91,6 @@ namespace octet {
         debugBezBuff.push_back(pos);
       }
 
-    }
-
-    void generate_random_points() {
-      // set the number of points you want generated
-      num_points = 22;
-      for (int i = 0; i < num_points; i++) {
-        waypoints.push_back(vec3(RandomFloat(-1, 1), RandomFloat(-1, 1), 0));
-      }
-    }
-
-    float get_distance(vec3 a, vec3 b) {
-      return sqrt(((b[0] - a[0]) * (b[0] - a[0])) // x
-        + ((b[1] - a[1]) * (b[1] - a[1])) // y
-        + ((b[2] - a[2]) * (b[2] - a[2]))); // z
-    }
-
-    void sort_waypoints() {
-      // sorting points by closest distance to each other
-      sorted_waypoints.push_back(waypoints.back());
-      waypoints.pop_back();
-      float shortest_dist, distance2;
-      int point_index;
-
-      while (!waypoints.empty()) {
-        shortest_dist = 0.0f;
-        distance2 = 0.0f;
-        point_index = 0;
-
-        for (int i = 0; i < waypoints.size(); i++) {
-          distance2 = get_distance(sorted_waypoints.back(), waypoints[i]);
-          if (distance2 < shortest_dist || shortest_dist <= 0) {
-            shortest_dist = distance2;
-            point_index = i;
-          }
-        }
-        sorted_waypoints.push_back(waypoints[point_index]);
-        waypoints.erase(waypoints.begin() + point_index);
-      }
-    }
-
-    void average_waypoints() {
-      // Averaging points back into original vector.
-      for (int i = 0; i < sorted_waypoints.size(); i++) {
-        if ((i + 2) == sorted_waypoints.size()) {
-          waypoints.push_back((sorted_waypoints[i] + sorted_waypoints[i + 1] + sorted_waypoints[0]) / 3);
-        }
-        else if ((i + 1) == sorted_waypoints.size()) {
-          waypoints.push_back((sorted_waypoints[i] + sorted_waypoints[0] + sorted_waypoints[1]) / 3);
-        }
-        else {
-          waypoints.push_back((sorted_waypoints[i] + sorted_waypoints[i + 1] + sorted_waypoints[i + 2]) / 3);
-        }
-      }
     }
 
     vec3 get_bezier_point(float t) {
---
+++ octet/src/examples/example_box/points_generator.h
@@ -0,0 +1,73 @@
+#pragma once
+
+namespace octet {
+  class points_generator {
+    std::vector<vec3> waypoints;
+    std::vector<vec3> sorted_waypoints;
+
+    float random_float(float a, float b) {
+      float random = ((float)rand()) / (float)RAND_MAX;
+      float diff = b - a;
+      float r = random * diff;
+      return a + r;
+    }
+
+    float get_distance(vec3 a, vec3 b) {
+      return sqrt(((b[0] - a[0]) * (b[0] - a[0])) // x
+        + ((b[1] - a[1]) * (b[1] - a[1])) // y
+        + ((b[2] - a[2]) * (b[2] - a[2]))); // z
+    }
+
+    void sort_waypoints() {
+      // sorting points by closest distance to each other
+      sorted_waypoints.push_back(waypoints.back());
+      waypoints.pop_back();
+      float shortest_dist, distance2;
+      int point_index;
+
+      while (!waypoints.empty()) {
+        shortest_dist = 0.0f;
+        distance2 = 0.0f;
+        point_index = 0;
+
+        for (int i = 0; i < waypoints.size(); i++) {
+          distance2 = get_distance(sorted_waypoints.back(), waypoints[i]);
+          if (distance2 < shortest_dist || shortest_dist <= 0) {
+            shortest_dist = distance2;
+            point_index = i;
+          }
+        }
+        sorted_waypoints.push_back(waypoints[point_index]);
+        waypoints.erase(waypoints.begin() + point_index);
+      }
+    }
+
+    void average_waypoints() {
+      // Averaging points back into original vector.
+      for (int i = 0; i < sorted_waypoints.size(); i++) {
+        if ((i + 2) == sorted_waypoints.size()) {
+          waypoints.push_back((sorted_waypoints[i] + sorted_waypoints[i + 1] + sorted_waypoints[0]) / 3);
+        }
+        else if ((i + 1) == sorted_waypoints.size()) {
+          waypoints.push_back((sorted_waypoints[i] + sorted_waypoints[0] + sorted_waypoints[1]) / 3);
+        }
+        else {
+          waypoints.push_back((sorted_waypoints[i] + sorted_waypoints[i + 1] + sorted_waypoints[i + 2]) / 3);
+        }
+      }
+    }
+  public:
+    points_generator() {}
+
+    std::vector<vec3> generate_random_points(int num_points) {
+      // set the number of points you want generated
+      for (int i = 0; i < num_points; i++) {
+        waypoints.push_back(vec3(random_float(-1, 1), random_float(-1, 1), 0));
+      }
+      sort_waypoints();
+      average_waypoints();
+
+      return waypoints;
+    }
+  };
+}
